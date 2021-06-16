5132627a45dd52cbacf984730225a72525fdc72a
chirag-mehta900 <chirag.smartypantstech@gmail.com>
2021-06-16 17:10:53 +0530

    changes

--- src/Components/Config/Apis.js
+++ src/Components/Config/Apis.js
@@ -1,5 +1,5 @@
-export const SERVER_URL = "http://192.168.29.121:8080/";
-// export const SERVER_URL = "https://api.herbshire.in/";
+// export const SERVER_URL = "http://192.168.29.121:8080/";
+export const SERVER_URL = "https://api.herbshire.in/";
 export const PRODUCT_URL = SERVER_URL + "product";
 export const PRODUCT_DELETE_URL = SERVER_URL + "product?productID=";
 export const UPLOAD_FILE_URL = SERVER_URL + "api/storage/uploadMultipleFile";
